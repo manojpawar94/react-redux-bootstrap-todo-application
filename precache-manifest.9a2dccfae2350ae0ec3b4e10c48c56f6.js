d07a50553884976989147cfa9274d111bdfc2b12
Manoj Pawar <mmpawar94@gmail.com>
2020-08-23 09:00:52 +0530

    Updates

---
+++ precache-manifest.9a2dccfae2350ae0ec3b4e10c48c56f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a503cb883506bb527148468327375f0",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "5c52896ac6bb90686361",
+    "url": "/todoapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "68496eadde0c4bcd5fa1",
+    "url": "/todoapp/static/js/2.066024c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.066024c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c52896ac6bb90686361",
+    "url": "/todoapp/static/js/main.e5c27798.chunk.js"
+  },
+  {
+    "revision": "6ca5f068bc6441d15faa",
+    "url": "/todoapp/static/js/runtime-main.ce279410.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
